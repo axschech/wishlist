6cd1c5dd8b10cd42dec8fbecec381f75eb28b85b
Alex Schechter <ax.schech@gmail.com>
2016-05-17 02:13:25 -0400

    workin on it

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import create_engine, Column, Integer, String. ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -21,12 +21,22 @@ class Game(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
 
+class User_Game(Base):
+    __tablename__ = 'users_games'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="users_games")
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship("Game", back_populates="users_games")
+
 class Price(Base):
     __tablename__ = 'prices'
     id = Column(Integer, primary_key=True)
-    game_id = Column(Integer)
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship("Game", back_populates="prices")
     price = Column(Integer)
-    platform_id = Column(Integer)
+    platform_id = Column(Integer, ForeignKey('platforms.id'))
+    platform = relationship("Platform", back_populates="prices")
 
 class Platform(Base):
     __tablename__ = 'platforms'
