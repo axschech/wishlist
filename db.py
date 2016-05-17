21770b5edaeb7e1c9490517788d94d7c8d73648b
Alex Schechter <ax.schech@gmail.com>
2016-05-17 16:54:45 -0400

    got schema and seeding

--- db.py
+++ db.py
@@ -1,10 +1,24 @@
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+import os
+
+from sqlalchemy import create_engine, Table, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref, sessionmaker
+
+try:
+    os.remove('db.db')
+except OSError:
+    pass
 
 engine = create_engine('sqlite:///db.db', echo=True)
 Base = declarative_base()
+
+print "\Schema\n"
+
+association_table = Table('users_games', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('game_id', Integer, ForeignKey('games.id'))
+)
 
 class User(Base):
     __tablename__ = 'users'
@@ -12,31 +26,24 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+    games = relationship('Game', secondary=association_table, backref="games")
 
-    def __repr__(self):
-       return "<User(name='%s', fullname='%s', password='%s')>" % (
-                            self.name, self.fullname, self.password)
+    # def __repr__(self):
+    #    return "<User(name='%s', name='%s', password='%s')>" % (
+    #                         self.name, self.name, self.password)
 class Game(Base):
     __tablename__ = 'games'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-
-class User_Game(Base):
-    __tablename__ = 'users_games'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship("User", back_populates="users_games")
-    game_id = Column(Integer, ForeignKey('games.id'))
-    game = relationship("Game", back_populates="users_games")
+    price_id = Column(Integer, ForeignKey('prices.id'))
+    price = relationship("Price")
 
 class Price(Base):
     __tablename__ = 'prices'
     id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey('games.id'))
-    game = relationship("Game", back_populates="prices")
     price = Column(Integer)
     platform_id = Column(Integer, ForeignKey('platforms.id'))
-    platform = relationship("Platform", back_populates="prices")
+    platform = relationship("Platform")
 
 class Platform(Base):
     __tablename__ = 'platforms'
@@ -45,3 +52,19 @@ class Platform(Base):
 
 
 Base.metadata.create_all(engine)
+
+print "\nSeeding\n"
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+games = [
+    Game(name="A Game", price=Price(price="150", platform=Platform(name="Steam")))
+]
+user = User(name="Alex", password="words", email="ax.schech@gmail.com", games=games)
+session.add(user)
+session.commit()
+
+for instance in session.query(User).all():
+    for game in instance.games:
+        print game.name
